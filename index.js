615218126d16e0c0ad4830846e626e23d8144622
Chetna <cmaske580@gmail.com>
2024-05-27 16:16:12 +0530

    html,css

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 let slide = document.querySelectorAll(".patientReview");
+ let card = document.querySelectorAll(".card");
+ let closeBtn = document.getElementById("closeBtn");
+ let connectBtn = document.getElementById("connectBtn");
 
-let count = 0;
 
-slide.forEach(function(slides, index){
-   slides.style.left=`${index * 100}%`
-})
+ let count = 0;
 
-function myFun(){
-   slide.forEach(function(curVal){
-       curVal.style.transform=`translateX(-${count * 100}%)`
-   })
-}
-setInterval(function(){
-   count++;
-   if(count == slide.length){
-       count=0;
-   }
-   myFun()
-}, 2000)
+ slide.forEach(function(slides, index){
+    slides.style.left=`${index * 100}%`
+ })
 
+ function myFun(){
+    slide.forEach(function(curVal){
+        curVal.style.transform=`translateX(-${count * 100}%)`
+    })
+ }
+ setInterval(function(){
+    count++;
+    if(count == slide.length){
+        count=0;
+    }
+    myFun()
+ }, 2000)
+
+  connectBtn.addEventListener("click", function(){
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(email.value == "" && pass.value == ""){
+        alert("Enter Details")
+    }else{
+        alert("You Logged IN")
+    }
+ })
