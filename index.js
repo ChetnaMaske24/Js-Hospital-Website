4046ed9cc36517293589a262c89b3ae03c4ddf44
Chetna <cmaske580@gmail.com>
2024-05-28 15:12:17 +0530

    js

--- index.js
+++ index.js
@@ -23,7 +23,24 @@ let slide = document.querySelectorAll(".patientReview");
     myFun()
  }, 2000)
 
-  connectBtn.addEventListener("click", function(){
+ card.forEach(function(cards){
+    cards.addEventListener("click", function(){
+        console.log(cards);
+        document.querySelector(".detail").style.display="block"
+        document.querySelector(".content").innerHTML=`
+        <img src=${cards.firstElementChild.src} alt="">
+        <div class="contentText">
+                    <h1>Alexa Zoan</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ex accusantium eius magnam, esse repellat veritatis, consectetur aut perspiciatis dolor labore sapiente.</p>
+        </div>
+        `
+       closeBtn.addEventListener("click", function(){
+        document.querySelector(".detail").style.display="none"
+       })
+    })
+ })
+
+ connectBtn.addEventListener("click", function(){
     let email = document.getElementById("email");
     let pass = document.getElementById("pass");
 
